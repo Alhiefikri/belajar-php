a8b23f80a5f16d3a8797eec03b630706ab373885
alhiefikri <alhiepedia@gmail.com>
2024-09-25 14:48:19 +0800

    first commit

---
+++ pengenalan/index.php
@@ -0,0 +1,22 @@
+<?php
+// pengenalan materi
+echo "materi pengenalan <br>";
+echo "materi pengenalan";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengenalan Materi</title>
+</head>
+
+<body>
+    <h1><?php echo "pengenalan materi php" ?></h1>
+    <h1><?= "pengenalan materi php" ?></h1>
+</body>
+
+</html>
